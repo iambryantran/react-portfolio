943a9a4f308516485f128d115f8f9c71314b8d4f
iambryantran <139257318+iambryantran@users.noreply.github.com>
2024-01-25 18:15:03 -0600

    navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
+import AboutMe from './components/AboutMe.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Contact from './components/Contact.jsx';
+import Resume from './components/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'about',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 );
