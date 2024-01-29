b4a14f0fa8db77820f92792bc7835fe0786c63ee
iambryantran <139257318+iambryantran@users.noreply.github.com>
2024-01-29 15:45:31 -0600

    added chosen projects to the portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,14 +1,28 @@
 // Create card component
 // Map over an array of objects (projects)
-const AboutMe = () => {
+import React from 'react';
+import Card from './Card';
+
+const Portfolio = () => {
+    const projects = [
+        { id: 1, title: 'Tips', url: '', githubLink: 'https://github.com/iambryantran/tips'},
+        { id: 2, title: 'E-commerce Backend', url: '', githubLink: 'https://github.com/iambryantran/ecommerce-backend'},
+        { id: 3, title: 'Employee Tracker', url: '', githubLink: 'https://github.com/iambryantran/employee-tracker'},
+        { id: 4, title: 'Note Taker', url: '', githubLink: 'https://github.com/iambryantran/note-taker'},
+        { id: 5, title: 'README Generator', url: '', githubLink: 'https://github.com/iambryantran/readme-generator'},
+        { id: 6, title: 'Game Release Calendar', url: '', githubLink: 'https://github.com/iambryantran/game-release-calendar'},
+    ];
+
     return (
-        <div>
-            <h1>About Me</h1>
-            {/* Add profile picture to src/assets and update below */}
-            <img src="/path/to/profile-picture.png" alt="Profile Picture" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum:</p>
+        <div className="portfolio">
+            <h2>Portfolio</h2>
+            {projects.map((project) => (
+                <div className="card" key={project.id}>
+                    <Card title={project.title}/>
+                </div>
+            ))}
         </div>
     );
 };
 
-export default AboutMe;
+export default Portfolio;
